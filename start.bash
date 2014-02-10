490e62aed9fbc06777dbad89c5fa219f83c78a26
Øyvind Skaar <me@odots.org>
2014-02-10 15:56:54 +0000

    require existing VOLUME with ghost content/ dir.

--- start.bash
+++ start.bash
@@ -6,18 +6,23 @@
 
 GHOST="/ghost"
 OVERRIDE="/ghost-override"
-DATA="content/data"
+# DATA="content/data"
 
 cd "$GHOST"
 
-# Symlinks files.
-if [[ -n $(ls -A "$OVERRIDE") ]]; then
-  cp -frs "$OVERRIDE/*" "$GHOST" 2> /dev/null
-fi
+# # Symlinks files.
+# if [[ -n $(ls -A "$OVERRIDE") ]]; then
+#   cp -frs "$OVERRIDE/*" "$GHOST" 2> /dev/null
+# fi
+#
+# # Symlink data directory.
+# mkdir -p "$OVERRIDE/$DATA"
+# rm -fr "$DATA"
+# ln -s "$OVERRIDE/$DATA" content
 
-# Symlink data directory.
-mkdir -p "$OVERRIDE/$DATA"
-rm -fr "$DATA"
-ln -s "$OVERRIDE/$DATA" content
+# No, lets instead, just assume we have an existing content directory in our volume
+mv content content-old
+ln -s "$OVERRIDE/content" content
+
 
 npm start
